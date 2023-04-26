6608fb0a10f2f63a7778957b69bf9e4cc1d1c318
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-04-26 11:01:06 +0300

    commit

--- MVC/JobLinq/Controllers/HomeController.cs
+++ MVC/JobLinq/Controllers/HomeController.cs
@@ -30,6 +30,11 @@ namespace JobLinq.Controllers
         {
             return View();
         }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
 
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
